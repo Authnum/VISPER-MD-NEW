e55bb151c21ff547f610c638362150a36f2e1c6b
Authnum <125637307+Authnum@users.noreply.github.com>
2025-07-06 08:10:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&W4J1zRLC#6Le5q3YYDmXAyMD3SegG4WYc-wUhQ8hmFRXKL6OoV3s' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
